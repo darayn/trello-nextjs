79b99123e758de333a43fb6f634b33c7b28c5feb
Dhanesh Phulphagar <41522314+darayn@users.noreply.github.com>
2023-06-20 00:22:45 +0530

    add zustand store

--- appwrite.ts
+++ appwrite.ts
@@ -1,9 +1,9 @@
-import { Account, Client, Client, Databases, ID, Storage } from "appwrite";
+import { Account, Client, Databases, ID, Storage } from "appwrite";
 
 const client = new Client();
 client
   .setEndpoint("https://cloud.appwrite.io/v1")
-  .setProject(process.env.NEXT_PUBLIC_APPWRITE_PROJECT_ID);
+  .setProject(process.env.NEXT_PUBLIC_APPWRITE_PROJECT_ID!);
 
 const database = new Databases(client);
 const account = new Account(client);
--- components/Board.tsx
+++ components/Board.tsx
@@ -1,13 +1,21 @@
+"use client";
 import { DragDropContext, Droppable } from "react-beautiful-dnd";
-import { Client } from "appwrite";
+import { useEffect } from "react";
+import { getTodosGroupedByColumns } from "@/lib/getTodosGroupedByColumn";
+import { useBoardStore } from "./store/BoardStore";
 
 function Board() {
+  const getBoard = useBoardStore((state) => state.getBoard);
+  useEffect(() => {
+    getBoard();
+  }, [getBoard]);
   return (
-    <DragDropContext>
-      <Droppable droppableId="board" direction="horizontal" type="column">
-        {(provided) => <div>{/* {rendering all the columns} */}</div>}
-      </Droppable>
-    </DragDropContext>
+    <h1>Hello</h1>
+    // <DragDropContext>
+    //   <Droppable droppableId="board" direction="horizontal" type="column">
+    //     {(provided) => <div>{/* {rendering all the columns} */}</div>}
+    //   </Droppable>
+    // </DragDropContext>
   );
 }
 
